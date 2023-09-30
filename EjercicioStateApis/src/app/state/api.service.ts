e4987df95894fb2038007c3d56b37f8ef065feb9
adalbarran <ad.albarran@duocuc.cl>
2023-09-29 22:13:13 -0300

    FinalVersion

--- EjercicioStateApis/src/app/state/api.service.ts
+++ EjercicioStateApis/src/app/state/api.service.ts
@@ -1,17 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class ApiService{
 
   constructor(
     private http: HttpClient
   ) { }
+
+
+  
   getFilms():Observable<any>{
     return this.http.get('https://swapi.dev/api/films/')
   }
-  
+
 }
